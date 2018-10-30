4c667cc99b4a830b43f67daa5feeb39c0abc2304
dcho0124 <34409108+dcho0124@users.noreply.github.com>
2018-10-30 17:44:30 -0500

    OutReach completion
    Finished writing the OutReach Tele-OP code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/First/OutReachTestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/First/OutReachTestTeleOp.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.First;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -13,16 +12,14 @@ import com.qualcomm.robotcore.util.Range;
 @TeleOp(name= "OutReachTestTeleOp", group = "Linear OpMode")
 
 public class OutReachTestTeleOp extends LinearOpMode {
-        //Initializing The Robot
 
         private ElapsedTime runtime = new ElapsedTime();
-
-        //Declare Motors
         private DcMotor BackLeft;
         private DcMotor BackRight;
         private DcMotor FrontLeft;
         private DcMotor FrontRight;
-
+        private DcMotor OutputL;
+        private  DcMotor OutputR;
         private Servo HopperServo;
 
         @Override
@@ -35,16 +32,25 @@ public class OutReachTestTeleOp extends LinearOpMode {
             FrontRight = hardwareMap.dcMotor.get("FrontRight");
             BackLeft = hardwareMap.dcMotor.get("BackLeft");
             BackRight = hardwareMap.dcMotor.get("BackRight");
+            OutputL = hardwareMap.dcMotor.get("OutputL");
+            OutputR = hardwareMap.dcMotor.get("OutputR");
 
             FrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
             BackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+            OutputR.setDirection(DcMotorSimple.Direction.REVERSE);
+            OutputL.setDirection(DcMotorSimple.Direction.REVERSE);
 
             HopperServo = hardwareMap.servo.get("HopperServo");
+
 
             waitForStart();
             runtime.reset();
 
             while (opModeIsActive()) {
+
+                double positionHopperServo = HopperServo.getPosition();
+                telemetry.addData("HopperServo Position", positionHopperServo);
+                telemetry.update();
 
                 if (gamepad1.left_stick_x != 0 || gamepad1.left_stick_y != 0) /*If the left stick is not neutral...*/ {
                     double drive = -gamepad1.left_stick_y; //Set the Drive to the negative value of the y-axis value
@@ -66,6 +72,17 @@ public class OutReachTestTeleOp extends LinearOpMode {
                     FrontRight.setPower(0);
                     BackLeft.setPower(0);
                     BackRight.setPower(0);
+                }
+
+                if (gamepad1.right_trigger != 0){
+
+                    OutputL.setPower(gamepad1.right_trigger);
+                    OutputR.setPower(gamepad1.right_trigger);
+                }
+                if (gamepad1.right_trigger == 0){
+
+                    OutputL.setPower(0);
+                    OutputR.setPower(0);
                 }
             }
 
